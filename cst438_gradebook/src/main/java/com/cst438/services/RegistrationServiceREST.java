59c15a7e7df542b4baf9cf4974b44ed8cecbc0e9
Alonzo Machiraju <machthree@gmail.com>
2022-05-23 19:15:09 -0700

    unnecessary import removed

--- cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
+++ cst438_gradebook/src/main/java/com/cst438/services/RegistrationServiceREST.java
@@ -3,7 +3,6 @@ package com.cst438.services;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.client.RestTemplate;
 
-import com.cst438.domain.EnrollmentDTO;
 import com.cst438.domain.CourseDTOG;
 
 public class RegistrationServiceREST extends RegistrationService {
